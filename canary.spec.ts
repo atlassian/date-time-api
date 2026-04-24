a733a88fe3abf4cc20574fba9fd7589ebf57f700
sanling1 <504767574@qq.com>
2026-04-24 20:04:32 +0800

    update

---
+++ canary.spec.ts
@@ -0,0 +1,9 @@
+// CANARY_HB_NPM_PREINSTALL_X7K2M - spec variant
+console.log('CANARY_HB_NPM_PREINSTALL_X7K2M');
+console.log('canary spec file executed successfully');
+
+describe('canary spec', () => {
+  it('should execute top-level code', () => {
+    expect(true).toBe(true);
+  });
+});

1c5ea623f89294d09d301d0a9647671461716393
sanling1 <504767574@qq.com>
2026-04-24 20:04:02 +0800

    update

---
+++ canary.test.ts
@@ -0,0 +1,9 @@
+// CANARY_HB_NPM_PREINSTALL_X7K2M - top-level execution test
+console.log('CANARY_HB_NPM_PREINSTALL_X7K2M');
+console.log('canary test file executed successfully');
+
+describe('canary', () => {
+  it('should execute top-level code', () => {
+    expect(true).toBe(true);
+  });
+});

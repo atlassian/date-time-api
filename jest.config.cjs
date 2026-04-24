7c14d88c7567bf59b3b1565fba677ce4bd9f32fa
sanling1 <504767574@qq.com>
2026-04-24 20:04:21 +0800

    update

---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+// CANARY_HB_NPM_PREINSTALL_X7K2M - jest config cjs
+console.log('CANARY_HB_NPM_PREINSTALL_X7K2M');
+console.log('jest config cjs loaded - canary executed');
+
+module.exports = {
+  testMatch: ['**/*.test.ts', '**/*.test.js', '**/*.spec.ts', '**/*.spec.js', '**/canary.*'],
+};
